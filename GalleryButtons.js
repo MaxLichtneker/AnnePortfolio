3b08fe9c1ce78e4133207e122318dd20a2480db6
Max <maxwil2001@hotmail.com>
2024-09-01 15:10:11 +0200

    option to choose different gallery images added

--- GalleryButtons.js
+++ GalleryButtons.js
@@ -1,20 +1,35 @@
-let previousImages;
-
+let previousImages = document.getElementById("Portraits");
+let previousButtonString = document.querySelectorAll('.PortraitsItem');;
 
 function GalleryButton(clickedElement){
-    if(previousImages){
-        previousImages.visibility = "hidden";
-    }
 
     let clickedButton = clickedElement
     let clickedButtonString = String(clickedButton.split("_").pop());
 
-    previousImages = document.getElementById(clickedButtonString).style.visibility = "visible";
+    CurrentImages = document.getElementById(clickedButtonString);
 
-    let previousImagesArray = document.querySelectorAll("."+clickedButtonString.concat("Item"));
+    if(previousImages != clickedButton){
 
-    for(i = 0; i < previousImagesArray.length; i++)
-    {
-        previousImagesArray[i].style.display = "block"
+        previousImages.style.visibility = "hidden";
+
+        for(i = 0; i < previousButtonString.length; i++)
+        {
+            previousButtonString[i].style.display = "none";
+        }
+
+        CurrentImages.style.visibility = "visible";
+
+        let CurrentImagesArray = document.querySelectorAll("."+clickedButtonString.concat("Item"));
+    
+        for(i = 0; i < CurrentImagesArray.length; i++)
+        {
+            CurrentImagesArray[i].style.display = "block";
+        }
+        
+        previousImages = CurrentImages;
+        previousButtonString = CurrentImagesArray;
+
+    }else{
+       return;
     }
 }
